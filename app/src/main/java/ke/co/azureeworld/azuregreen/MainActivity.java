67ef9ebd6b5f1d60a350982c6dcb1cced74bb4c7
AugustineKadima <kadimasam@gmail.com>
2021-11-04 22:55:32 +0300

    Add create account

--- app/src/main/java/ke/co/azureeworld/azuregreen/MainActivity.java
+++ app/src/main/java/ke/co/azureeworld/azuregreen/MainActivity.java
@@ -2,13 +2,36 @@ package ke.co.azureeworld.azuregreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ke.co.azureeworld.azuregreen.setup.CreateAccountActivity;
+import ke.co.azureeworld.azuregreen.setup.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_login, create_account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = (Button) findViewById(R.id.btn_login_main);
+        create_account = (Button) findViewById(R.id.btn_create_account_main);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/ke/co/azureeworld/azuregreen/setup/LoginActivity.java
@@ -0,0 +1,16 @@
+package ke.co.azureeworld.azuregreen.setup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ke.co.azureeworld.azuregreen.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_farmer);
+    }
+}
